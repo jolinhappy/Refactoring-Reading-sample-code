7ae4a2587a2dbf99f3391d3422d9b88a04b484ce
jolinhappy <jolinhappy1990@hotmail.com>
2023-02-05 00:03:36 +0800

    1. 進行Encapsulate Variable 封裝變數

--- EncapsulateRecord1.js
+++ EncapsulateRecord1.js
@@ -5,4 +5,11 @@ const organization = {
 }
 let result = '';
 // 讀取時
-result += `<h1>${organization.name}</h1>`;
+result += `<h1>${getRawDataOfOrganization().name}</h1>`;
+
+// 用好查詢的名稱命名這個取得紀錄的函式
+function getRawDataOfOrganization() {
+  return organization;
+}
+// 想要進行更新的話
+getRawDataOfOrganization().name = 'Hebe';
