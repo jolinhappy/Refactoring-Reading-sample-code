97f73a850b98fb0d9950465013f56494edaae48e
jolinhappy <jolinhappy1990@hotmail.com>
2023-02-05 17:03:49 +0800

    2. 把get方法放入賦值式右方

--- ReplaceTempWithQuery.js
+++ ReplaceTempWithQuery.js
@@ -4,9 +4,22 @@ class Order {
     this.item = item;
   }
   get price() {
-    const basePrice = this._quantity * this._item.price;
+    const basePrice = this.basePrice;
     var discountFactor = 0.98;
     if (basePrice > 1000) discountFactor -= 0.03;
     return basePrice * discountFactor;
   }
+  get basePrice() {
+    return this._quantity * this.item.price
+  }
 }
+
+//  使用時
+const order = new Order(1, {
+  name: 'book',
+  price: 200
+});
+
+// 取得原價和折扣價
+console.log(order.price);
+console.log(order.basePrice);
