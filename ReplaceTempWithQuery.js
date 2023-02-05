b0bf6f4d1c7d2620129886eb436159d1eae7109a
jolinhappy <jolinhappy1990@hotmail.com>
2023-02-05 17:04:57 +0800

    3. 測試並進行Inline Variable

--- ReplaceTempWithQuery.js
+++ ReplaceTempWithQuery.js
@@ -4,10 +4,9 @@ class Order {
     this.item = item;
   }
   get price() {
-    const basePrice = this.basePrice;
     var discountFactor = 0.98;
-    if (basePrice > 1000) discountFactor -= 0.03;
-    return basePrice * discountFactor;
+    if (this.basePrice > 1000) discountFactor -= 0.03;
+    return this.basePrice * discountFactor;
   }
   get basePrice() {
     return this._quantity * this.item.price
