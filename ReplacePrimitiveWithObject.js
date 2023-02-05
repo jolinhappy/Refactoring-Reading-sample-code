637aff59e200e8e98b9af52aa00aae7543d8c6b1
jolinhappy <jolinhappy1990@hotmail.com>
2023-02-05 11:47:25 +0800

    2,3. 幫資料值建立簡單值類別，並修改相關的存取函式，改成使用這個新建立的值類別

--- ReplacePrimitiveWithObject.js
+++ ReplacePrimitiveWithObject.js
@@ -1,13 +1,23 @@
+
+class Priority {
+  constructor(value) {
+    this._value = value;
+  }
+  toString() {
+    return this._value
+  }
+}
+
 class Order {
   constructor(data) {
     this._priority = data.priority;
     // 更多初始化
   }
   get priority() {
-    return this._priority;
+    return this._priority.toString();
   }
   set priority(aString) {
-    this._priority = aString;
+    this._priority = new Priority(aString);
   }
 }
 
@@ -18,7 +28,7 @@ const orderList = [
   { priority: 'rush' },
   { priority: 'high' },
 ];
-const orders = orderList.map(data => new Order(data));
+const orders = orderList.map(data => new Order({priority: new Priority(data.priority)}));
 
 // 使用方
 highPriority = orders.filter(o => 'high' === o.priority || 'rush' === o.priority).length;
